3e437a28a3eb8d7759bbd44a67c3e5eba14cfe15
Tanguy Lefort <tanguy_l@outlook.fr>
2020-03-08 11:40:35 +0100

    Initialize the package structure
    -Doc for the sphinx documentation
    -beamer for the beamer presentation
    -chaoseverywhere for the actual code
    -report for the jupyter presenting our package ie demo folder

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from chaoseverywhere import __version__ as current_version
+
+setup(
+  name='chaoseverywhere',
+  version=current_version,
+  description='Visualization of the Mandelbrot set and the logistic map',
+  url='http://github.com/tanglef/chaoseverywhere.git',
+  author='Ophelie Coiffier ; Tanguy Lefort ; Ibrahim Gaizi',
+  author_email='tanguy.lefort@etu.umontpellier.fr',
+  license='MIT',
+  packages=['chaoseverywhere'],
+  zip_safe=False
+)
