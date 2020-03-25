b58ef93e4e69908cbbfbecb1775d4e86635d2f11
Tanguy Lefort <tanguy_l@outlook.fr>
2020-03-25 21:55:35 +0100

    Add 3D animation, test included and some draft
    - 2D Mandelbrot with loops for colors
    - 3D animation and production of a folder + 200 frames
    - Use of ffmpeg and mayavi

--- chaoseverywhere/tests/test_chaoseverywhere.py
+++ chaoseverywhere/tests/test_chaoseverywhere.py
@@ -1,15 +1,20 @@
 import os.path
 import sys
+import numpy as np
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
 import chaoseverywhere as chaos
 
+
 def test_mandel():
     test = chaos.Mandelbrot_disp(-.5, 0, 1,t_max=10,precision=20).mandelbrot()
-    assert test[0,0]==True
+    assert test[0,0] == True
 
 def test_logi():
     test = chaos.logistic(3.6, 0.001)
-    assert test == 0.0035964
-    
+    assert np.isclose(0.0035964,test)
+
+def test_mandel_loop():
+    test = chaos.Mandelbrot_disp(-.5, 0, 1).mandel_loop()[0,0]
+    assert np.isclose(test,1.5198773448773448)
--- script.py
+++ script.py
@@ -1,5 +1,8 @@
 import chaoseverywhere as chaos
 
-chaos.Mandelbrot_disp(-.5, 0, 1).disp_mandel()
-chaos.Mandelbrot_disp(-.5, 0, 1, t_max = 50, precision = 350).animate_mandel_plt()
-chaos.logistic(3.6, 0.001)
+#chaos.Mandelbrot_disp(-.5, 0, 1).disp_mandel()
+#chaos.Mandelbrot_disp(-.5, 0, 1, t_max = 50, precision = 350).animate_mandel_plt()
+#print(chaos.logistic(3.6, 0.001))
+
+chaos.Mandelbrot_disp(-.5,0,1.5).anim_pics_mandel()
+
