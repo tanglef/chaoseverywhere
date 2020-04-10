2864f4a8b42745de088f69cf131fa6f577fae415
Tanguy Lefort <tanguy_l@outlook.fr>
2020-04-10 16:57:45 +0200

    remove ffmpeg and replace it with imagio

--- examples/plot_zoom.py
+++ examples/plot_zoom.py
@@ -12,13 +12,11 @@ import chaoseverywhere as chaos
 import matplotlib.pyplot as plt
 import os
 import matplotlib.animation as animation
+import imageio
 # sphinx_gallery_thumbnail_path = '../doc/_build/html/_images/thumbnail.svg'
 
 im_init = chaos.Mandelbrot_disp(-.5,0,1.5)
 im_init = im_init.mandelbrot()
-
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=15, metadata=dict(artist='Me'))
 
 fig = plt.figure()
 im = plt.imshow(im_init, cmap='bone')
@@ -28,10 +26,12 @@ results_dir = '../doc/_build/html/_images/'
 sample_file_name = "Mandel_zoom"
 os.makedirs(results_dir, exist_ok=True)
 
+images=[]
 def animate(i):
     im.set_data(chaos.Mandelbrot_disp(-1, -.3, 0.4-i/300,
             t_max=100,
             precision=400).mandelbrot())
+    images.append(im)
     if(i==130):
         plt.savefig(results_dir + 'thumbnail.svg')
     return im,
@@ -39,7 +39,7 @@ def animate(i):
 
 anim = animation.FuncAnimation(fig, animate, frames=150, interval=2,
                                        repeat=False, save_count=200)
-anim.save(results_dir + sample_file_name + '.mp4', writer=writer)
+imageio.mimsave(results_dir + sample_file_name + '.gif', images, duration=.25)
 
 plt.close()
 
@@ -50,5 +50,5 @@ plt.close()
 # ----------------
 # .. raw:: html
 #
-#          <video controls src="../../../_build/html/_images/Mandel_zoom.mp4"></video>
+#          <img class='sphx-glr-single-img' src="../../../_build/html/_images/Mandel_zoom.gif"/>
 
