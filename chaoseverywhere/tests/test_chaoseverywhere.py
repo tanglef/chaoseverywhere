d3d16f4a1a46455a9618760138bece1f1f1dbe18
Tanguy Lefort <tanguy_l@outlook.fr>
2020-04-16 21:12:52 +0200

    add tests for images

--- chaoseverywhere/tests/test_chaoseverywhere.py
+++ chaoseverywhere/tests/test_chaoseverywhere.py
@@ -7,7 +7,15 @@ warnings.filterwarnings("ignore", category=RuntimeWarning)
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + (os.path.sep + '..')*2)
 
 import chaoseverywhere as chaos
-
+result_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..", "doc", "_images")
+print(result_dir)
+import hashlib
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def test_mandel():
     test = chaos.Mandelbrot_disp(-.5, 0, 1,t_max=10,precision=20).mandelbrot()
@@ -26,3 +34,12 @@ def test_mandel_branch_points():
 
 def test_bifurcation():
     assert np.isclose(chaos.bifurcation(show=False)[0][18], 1.0054005400540054)
+
+def test_image_3d_transform():
+    assert md5(os.path.join(result_dir, "3d_transform.svg")) == "fea6773eb100bb741b7d8b81502da084"
+
+def test_image_3d_vision():
+    assert md5(os.path.join(result_dir, "3d_vision.svg")) == "93c1e43d3426df6314feef637abdc29f"
+
+def test_image_puiss4():
+    assert md5(os.path.join(result_dir, "puiss4.svg")) == "678bad5eb2c13ee28c9ea21136291de5"
