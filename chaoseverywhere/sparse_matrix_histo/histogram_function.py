48425c822bd8cff56c9be737ecfb32b6de8cbe9a
Tanguy Lefort <tanguy_l@outlook.fr>
2020-04-19 16:41:58 +0200

    fix inits for histogram

--- chaoseverywhere/sparse_matrix_histo/histogram_function.py
+++ chaoseverywhere/sparse_matrix_histo/histogram_function.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-
+from chaoseverywhere.sparse_matrix_histo import Mandelbrot_disp
 
 def histogram(x=-.5, y=0, window=1.5):
     mandel = Mandelbrot_disp(-.5,0,1.5).mandel_loop()
     data = np.array(np.unique(mandel,return_counts=True)).T
     plt.style.use('ggplot')
     plt.figure()
-    plt.bar(data[:,0],data:[:,1], width=.15, align= 'edge')
+    plt.bar(data[:,0],data[:,1], width=.15, align= 'edge')
     plt.show()
