9dcd63801f8c3e969580ef3149a3ffbd629a39f4
Tanguy <tanguy_l@outlook.fr>
2020-05-10 23:28:13 +0200

    remove unused lines

--- benchmarks/snake_profile.py
+++ benchmarks/snake_profile.py
@@ -7,9 +7,3 @@ import chaoseverywhere as chaos
 chaos.Mandelbrot_disp(-.5,0,1, t_max=300).mandelbrot()
 chaos.Mandelbrot_disp(-.5,0,1, t_max=300).mandel_loop()
 chaos.bifurcation(show=False)
-
-#cProfile.run('chaos.Mandelbrot_disp(-.5,0,1, t_max=300).mandelbrot()', os.path.join('.','mandel.prof'))
-#cProfile.run('chaos.Mandelbrot_disp(-.5,0,1, t_max=300).mandel_loop()', 'mandel_loop.prof')
-#cProfile.run('chaos.bifurcation(show=False)', 'bifurcation.prof')
-
-
